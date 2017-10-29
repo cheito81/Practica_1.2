c31e37994335f253497d4666afb35ed5df888ec7
webapps <webapps@webapps>
2017-10-29 15:50:33 +0100

    Exercicie 2

--- src/ej_1_nom_may_vocals/Practica_pt1_2_ex_2.java
+++ src/ej_1_nom_may_vocals/Practica_pt1_2_ex_2.java
@@ -14,53 +14,60 @@ import java.util.Scanner;
  * @author alumne
  */
 public class Practica_pt1_2_ex_2 {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         run();
     }
-    
-    private static void run(){
+
+    private static void run() {
         Scanner scan = new Scanner(System.in);
         Map listChars;
         System.out.println("Enter a word: ");
-        String word = scan.next();
-        
+        String word = scan.nextLine();
+
         word = vowelsToUpperCase(word);
+        
         word = consonantsToLowerCase(word);
-        //listChars = countChars(word);
+        listChars = countChars(word);
+        System.out.println("Result word: "+word);
+        System.out.println("Count of chars in the word: "+listChars.toString());
     }
-    
-    private static int countUpperCase(String word){
-        int upper=0;
-        for(int i=0; i<word.length(); i++)
-        {
-            if((word.charAt(i)>='A') && (word.charAt(i)<='Z'))upper++;
-        }
-        return upper;
-    }
-    
-    private static String consonantsToLowerCase(String word){
+
+    private static String consonantsToLowerCase(String word) {
         for (int i = 0; i < word.length(); i++) {
-            if((word.charAt(i)!='A') && (word.charAt(i)!='E') && (word.charAt(i)!='I') && (word.charAt(i)!='O') && (word.charAt(i)!='U')){
-                word = word.replace(word.charAt(i), word.toUpperCase().charAt(i));
+            if ((word.charAt(i) != 'A') && (word.charAt(i) != 'E') && (word.charAt(i) != 'I') && (word.charAt(i) != 'O') && (word.charAt(i) != 'U')) {
+                word = word.replace(word.charAt(i), word.toLowerCase().charAt(i));
             }
         }
         return word;
     }
-    
-    private static String vowelsToUpperCase(String word){
-        word = word.replaceAll("a","A");
-        word = word.replaceAll("e","E");
-        word = word.replaceAll("i","I");
-        word = word.replaceAll("o","O");
-        word = word.replaceAll("u","U");
+
+    private static String vowelsToUpperCase(String word) {
+        word = word.replaceAll("a", "A");
+        word = word.replaceAll("e", "E");
+        word = word.replaceAll("i", "I");
+        word = word.replaceAll("o", "O");
+        word = word.replaceAll("u", "U");
+ 
         return word;
     }
-    
-    /*private static Map countChars(String word){
-        Map charsList = new HashMap()<>;
-        return charsList;
-    }*/
+
+    private static Map countChars(String word) {
+        String aux = word.replaceAll(" ","");
+        HashMap<Character, Integer> map = new HashMap();
+        
+        for (int i = 0; i < aux.length(); i++) {
+            char c = aux.charAt(i);
+            Integer val = map.get(c);
+            if (val != null) {
+                map.put(c, val + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return map;
+    }
 }
