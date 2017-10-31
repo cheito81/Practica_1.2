265000c03ea9f1e20e7e22670574d23dd2fd555a
Windows <Windows@Windows-PC>
2017-10-31 15:39:29 +0100

    Test1

---
+++ test/ADN_ARNTest.java
@@ -0,0 +1,37 @@
+import ej_3_ADN.ADN_ARN;
+import org.junit.Test;
+import static org.junit.Assert.*;
+//imports per fer servir la paramtrització
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+/**
+ *
+ * @author alumne
+ */
+@RunWith(Parameterized.class)
+public class ADN_ARNTest {
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                {"atgcatcgatcag", "AUGCAUCGAUCAG"}, {"atgcatcgatcag", "AUGCAUCGAUCAG"}, {"atgcatcgatcag", "AUGCAUCGAUCAG"}
+           });
+    }
+    private String date;
+    private String expected;
+     
+    public ADN_ARNTest(String date,String expected) {
+        this.date=date;
+        this.expected=expected;
+    }
+    
+    
+    @Test
+    public void  validarFechaTest() {
+        ADN_ARN result=new ADN_ARN();
+        String res=result.adnToArn(this.date);
+        assertEquals(this.expected, res);  
+    }
+}
