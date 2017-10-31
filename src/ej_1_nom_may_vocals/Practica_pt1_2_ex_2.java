99daae1cb2cf5c034b3219c58de9ce5a1d66df2d
Windows <Windows@Windows-PC>
2017-10-31 13:30:54 +0100

    P1

--- src/ej_1_nom_may_vocals/Practica_pt1_2_ex_2.java
+++ src/ej_1_nom_may_vocals/Practica_pt1_2_ex_2.java
@@ -5,6 +5,10 @@
  */
 package ej_1_nom_may_vocals;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author alumne
@@ -15,7 +19,55 @@ public class Practica_pt1_2_ex_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        run();
     }
-    
+
+    private static void run() {
+        Scanner scan = new Scanner(System.in);
+        Map listChars;
+        System.out.println("Enter a word: ");
+        String word = scan.nextLine();
+
+        word = vowelsToUpperCase(word);
+        
+        word = consonantsToLowerCase(word);
+        listChars = countChars(word);
+        System.out.println("Result word: "+word);
+        System.out.println("Count of chars in the word: "+listChars.toString());
+    }
+
+    private static String consonantsToLowerCase(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if ((word.charAt(i) != 'A') && (word.charAt(i) != 'E') && (word.charAt(i) != 'I') && (word.charAt(i) != 'O') && (word.charAt(i) != 'U')) {
+                word = word.replace(word.charAt(i), word.toLowerCase().charAt(i));
+            }
+        }
+        return word;
+    }
+
+    private static String vowelsToUpperCase(String word) {
+        word = word.replaceAll("a", "A");
+        word = word.replaceAll("e", "E");
+        word = word.replaceAll("i", "I");
+        word = word.replaceAll("o", "O");
+        word = word.replaceAll("u", "U");
+ 
+        return word;
+    }
+
+    private static Map countChars(String word) {
+        String aux = word.replaceAll(" ","");
+        HashMap<Character, Integer> map = new HashMap();
+        
+        for (int i = 0; i < aux.length(); i++) {
+            char c = aux.charAt(i);
+            Integer val = map.get(c);
+            if (val != null) {
+                map.put(c, val + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return map;
+    }
 }
