acc246b2ca71beb2a24bbbd6a3b9639078790018
jose <jose@jose-equip>
2017-10-31 18:35:45 +0100

    ejercicio 4 hecho faltan test

--- src/ej_4_nucleotids/NucleotidsPrincipal.java
+++ src/ej_4_nucleotids/NucleotidsPrincipal.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
+import static jdk.nashorn.internal.objects.NativeArray.map;
 
 /**
  *
@@ -41,6 +42,76 @@ public class NucleotidsPrincipal {
      * runs the application in non-static mode.
      */	
 	private void run() {	
+            
+             String result="";
+            
+             HashMap<String, String> map = new HashMap<String, String>();
+ 
+    map.put("UCA","S"); // Serine
+   map.put("UCC" , "S");  // Serine
+ map.put("UCG" , "S");  // Serine
+ map.put("UCU" , "S");  // Serine
+ map.put("UUC" , "F");  // Phenylalanine
+ map.put("UUU" , "F");  // Phenylalanine
+ map.put("UUA" , "L");  // Leucine
+ map.put("UUG" , "L");  // Leucine
+ map.put("UAC" , "Y");  // Tyrosine
+ map.put("UAU" , "Y");  // Tyrosine
+ map.put("UAA" , "_");  // Stop
+ map.put("UAG" , "_");  // Stop
+ map.put("UGC" , "C");  // Cysteine
+map.put("UGU" , "C"); // Cysteine
+ map.put("UGA" , "_");  // Stop
+ map.put("UGG" , "W");  // Tryptophan
+ map.put("CUA" , "L");  // Leucine
+ map.put("CUC" , "L");  // Leucine
+ map.put("CUG" , "L");  // Leucine
+ map.put("CUU" , "L");  // Leucine
+ map.put("CCA" , "P");  // Proline
+ map.put("CAU" , "H");  // Histidine
+ map.put("CAA" , "Q");  // Glutamine
+ map.put("CAG" , "Q");  // Glutamine
+ map.put("CGA" , "R");  // Arginine
+ map.put("CGC" , "R");  // Arginine
+ map.put("CGG" , "R");  // Arginine
+ map.put("CGU" , "R");  // Arginine
+ map.put("AUA" , "I");  // Isoleucine
+ map.put("AUC" , "I");  // Isoleucine
+ map.put("AUU" , "I");  // Isoleucine
+ map.put("AUG" , "M");  // Methionine
+ map.put("ACA" , "T");  // Threonine
+ map.put("ACC" , "T");  // Threonine
+ map.put("ACG" , "T");  // Threonine
+ map.put("ACU" , "T");  // Threonine
+ map.put("AAC" , "N");  // Asparagine
+ map.put("AAU" , "N");  // Asparagine
+ map.put("AAA" , "K");  // Lysine
+ map.put("AAG" , "K");  // Lysine
+ map.put("AGC" , "S");  // Serine
+ map.put("AGU" , "S");  // Serine
+ map.put("AGA" , "R");  // Arginine
+ map.put("AGG" , "R");  // Arginine
+ map.put("CCC" , "P");  // Proline
+ map.put("CCG" , "P");  // Proline
+ map.put("CCU" , "P");  // Proline
+ map.put("CAC" , "H");  // Histidine
+ map.put("GUA" , "V");  // Valine
+ map.put("GUC" , "V");  // Valine
+ map.put("GUG" , "V");  // Valine
+ map.put("GUU" , "V");  // Valine
+ map.put("GCA" , "A");  // Alanine
+ map.put("GCC" , "A");  // Alanine
+ map.put("GCG" , "A");  // Alanine
+ map.put("GCU" , "A");  // Alanine
+ map.put("GAC" , "D");  // Aspartic Acid
+ map.put("GAU" , "D");  // Aspartic Acid
+ map.put("GAA" , "E");  // Glutamic Acid
+ map.put("GAG" , "E");  // Glutamic Acid
+ map.put("GGA" , "G");  // Glycine
+ map.put("GGC" , "G");  // Glycine
+ map.put("GGG" , "G"); // Glycine
+ map.put("GGU" , "G");  // Glycine)
+  
 		/* exit flag */
 		boolean exit = false;
 		/* menu option to execute */
@@ -55,13 +126,14 @@ public class NucleotidsPrincipal {
 				case 0: //exit
 					exit = true;  //set the exit flag.
 					break;
-				case 1: //list all
+				case 1: result = cadenaToAmino(map);
+                                         System.out.println("LOS AMINOACIDOS SON: "+result);
 					
 					break;
-				case 2: //find by id
-					
+				case 2: result = aminoToCadena(map);
+					System.out.println("Los nucleotidos son: "+result);
 					break;
-				case 3: //find by name
+				case 3:
 					
 					break;
 				default: //default or invalid option
@@ -94,143 +166,86 @@ public class NucleotidsPrincipal {
 		return option;
 	}
      
-     private String cadenaToAmino(){
-         Map<String, String> map = new HashMap<String, String>();
+     /**
+ *  pasar de nucleotidos a aminoacido
+ * @param map hashmap con todos los aminoacidos que existen
+ * @return result te devuelve los aminoacidos de los nucleotidos introducidos.
+ */
+     private String cadenaToAmino( HashMap<String, String> map){
+         
+         String valor ="";
+         String cadena="";
          String result="";
          
-   map.put("UCA","S"); // Serine
-  /*map.put("UCC" , "S"// Serine
-  "UCG" , "S", // Serine
-  "UCU" , "S", // Serine
-  "UUC" , "F", // Phenylalanine
-  "UUU" , "F", // Phenylalanine
-  "UUA" , "L", // Leucine
-  "UUG" , "L", // Leucine
-  "UAC" , "Y", // Tyrosine
-  "UAU" , "Y", // Tyrosine
-  "UAA" , "_", // Stop
-  "UAG" , "_", // Stop
-  "UGC" , "C", // Cysteine
-  "UGU" , "C", // Cysteine
-  "UGA" , "_", // Stop
-  "UGG" , "W", // Tryptophan
-  "CUA" , "L", // Leucine
-  "CUC" , "L", // Leucine
-  "CUG" , "L", // Leucine
-  "CUU" , "L", // Leucine
-  "CCA" , "P", // Proline
-  "CAU" , "H", // Histidine
-  "CAA" , "Q", // Glutamine
-  "CAG" , "Q", // Glutamine
-  "CGA" , "R", // Arginine
-  "CGC" , "R", // Arginine
-  "CGG" , "R", // Arginine
-  "CGU" , "R", // Arginine
-  "AUA" , "I", // Isoleucine
-  "AUC" , "I", // Isoleucine
-  "AUU" , "I", // Isoleucine
-  "AUG" , "M", // Methionine
-  "ACA" , "T", // Threonine
-  "ACC" , "T", // Threonine
-  "ACG" , "T", // Threonine
-  "ACU" , "T", // Threonine
-  "AAC" , "N", // Asparagine
-  "AAU" , "N", // Asparagine
-  "AAA" , "K", // Lysine
-  "AAG" , "K", // Lysine
-  "AGC" , "S", // Serine
-  "AGU" , "S", // Serine
-  "AGA" , "R", // Arginine
-  "AGG" , "R", // Arginine
-  "CCC" , "P", // Proline
-  "CCG" , "P", // Proline
-  "CCU" , "P", // Proline
-  "CAC" , "H", // Histidine
-  "GUA" , "V", // Valine
-  "GUC" , "V", // Valine
-  "GUG" , "V", // Valine
-  "GUU" , "V", // Valine
-  "GCA" , "A", // Alanine
-  "GCC" , "A", // Alanine
-  "GCG" , "A", // Alanine
-  "GCU" , "A", // Alanine
-  "GAC" , "D", // Aspartic Acid
-  "GAU" , "D", // Aspartic Acid
-  "GAA" , "E", // Glutamic Acid
-  "GAG" , "E", // Glutamic Acid
-  "GGA" , "G", // Glycine
-  "GGC" , "G", // Glycine
-  "GGG" , "G", // Glycine
-  "GGU" , "G");  // Glycine)
-         /**
-          *  "UCA" , "S", // Serine
-  "UCC" , "S", // Serine
-  "UCG" , "S", // Serine
-  "UCU" , "S", // Serine
-  "UUC" , "F", // Phenylalanine
-  "UUU" , "F", // Phenylalanine
-  "UUA" , "L", // Leucine
-  "UUG" , "L", // Leucine
-  "UAC" , "Y", // Tyrosine
-  "UAU" , "Y", // Tyrosine
-  "UAA" , "_", // Stop
-  "UAG" , "_", // Stop
-  "UGC" , "C", // Cysteine
-  "UGU" , "C", // Cysteine
-  "UGA" , "_", // Stop
-  "UGG" , "W", // Tryptophan
-  "CUA" , "L", // Leucine
-  "CUC" , "L", // Leucine
-  "CUG" , "L", // Leucine
-  "CUU" , "L", // Leucine
-  "CCA" , "P", // Proline
-  "CAU" , "H", // Histidine
-  "CAA" , "Q", // Glutamine
-  "CAG" , "Q", // Glutamine
-  "CGA" , "R", // Arginine
-  "CGC" , "R", // Arginine
-  "CGG" , "R", // Arginine
-  "CGU" , "R", // Arginine
-  "AUA" , "I", // Isoleucine
-  "AUC" , "I", // Isoleucine
-  "AUU" , "I", // Isoleucine
-  "AUG" , "M", // Methionine
-  "ACA" , "T", // Threonine
-  "ACC" , "T", // Threonine
-  "ACG" , "T", // Threonine
-  "ACU" , "T", // Threonine
-  "AAC" , "N", // Asparagine
-  "AAU" , "N", // Asparagine
-  "AAA" , "K", // Lysine
-  "AAG" , "K", // Lysine
-  "AGC" , "S", // Serine
-  "AGU" , "S", // Serine
-  "AGA" , "R", // Arginine
-  "AGG" , "R", // Arginine
-  "CCC" , "P", // Proline
-  "CCG" , "P", // Proline
-  "CCU" , "P", // Proline
-  "CAC" , "H", // Histidine
-  "GUA" , "V", // Valine
-  "GUC" , "V", // Valine
-  "GUG" , "V", // Valine
-  "GUU" , "V", // Valine
-  "GCA" , "A", // Alanine
-  "GCC" , "A", // Alanine
-  "GCG" , "A", // Alanine
-  "GCU" , "A", // Alanine
-  "GAC" , "D", // Aspartic Acid
-  "GAU" , "D", // Aspartic Acid
-  "GAA" , "E", // Glutamic Acid
-  "GAG" , "E", // Glutamic Acid
-  "GGA" , "G", // Glycine
-  "GGC" , "G", // Glycine
-  "GGG" , "G", // Glycine
-  "GGU" , "G"  // Glycine
-          */
-         System.out.println("Escribe cade de nucleotidos: ");
+         Scanner scan = new Scanner(System.in);
+
+                   System.out.println("Escribe cadena de nucleotidos: ");
+                   cadena= scan.next();
+                   cadena=cadena.trim();
+                   cadena=cadena.toUpperCase();
+                   int j=0;
+                   for(int i=0;i<cadena.length()-2 ;i=i + 3){
+                        j = i+3;
+                        
+                       String sSubCadena = cadena.substring(i, j);
+                       
+                        if (map.containsKey(sSubCadena)){
+                            valor+= map.get(sSubCadena);
+                             valor +=" ";
+                                      
+                        }
+                        else{
+                            System.out.println("No hay ningun aminoacido con estos nucleotidos.");
+                            System.out.println(sSubCadena);
+                        }
+                       
+                   };
+                   result = valor;
          return result;
      }
+     
+/**
+ *  pasar de aminoacido a nucleotidos
+ * @param map hashmap con todos los aminoacidos que existen
+ * @return result te devuelve la cadena de nucleotidos de los aminoacidos introducidos.
+ */
+    private String aminoToCadena(HashMap<String, String> map) {
+        
+       String valor ="";
+         String cadena="";
+         String result="";
+         
+         Scanner scan = new Scanner(System.in);
+
+                   System.out.println("Escribe nucleotidos: ");
+                   cadena= scan.next();
+                   cadena=cadena.trim();
+                   cadena=cadena.toUpperCase();
+                   int j=0;
+                   for(int i=0;i<cadena.length();i++){
+                        j = i+1;
+                        
+                       String sSubCadena = cadena.substring(i, j);
+                       
+                        if (map.containsValue(sSubCadena)){
+                            
+                            for (Map.Entry<String, String> entry : map.entrySet()) {
+                                String key = entry.getKey();
+                                valor+= key;
+                                
+                            }
+                                      
+                        }
+                        else{
+                            System.out.println("No hay ningun nucleotido con estos aminoacidos.");
+                            System.out.println(sSubCadena);
+                        }
+                       
+                   };
+                   result = valor;
+        
+        return result;
+    }
      
   
 }
